e674ca8c24c9aaec0fd44a56d36357756a05b5de
Christopher Peckover <christopher.peckover@gmail.com>
2017-10-02 18:18:41 -0400

    Logging storeId

--- src/main/java/com/gazorpazorp/service/StoreService.java
+++ src/main/java/com/gazorpazorp/service/StoreService.java
@@ -1,5 +1,7 @@
 package com.gazorpazorp.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,11 +10,14 @@ import com.gazorpazorp.model.Store;
 
 @Service
 public class StoreService {
+	Logger logger = LoggerFactory.getLogger(StoreService.class);
 
 	@Autowired
 	LCBOFeignClient lcboClient;
 	
 	public Store locateClosestStoreToCoords(double latitude, double longitude) {
-		return lcboClient.getStoresNearPoints(latitude, longitude).getResult().stream().findFirst().get();
+		Store store = lcboClient.getStoresNearPoints(latitude, longitude).getResult().get(0);
+		logger.warn("STORE_ID" + store.getId());
+		return store;
 	}
 }
